96fd68971f2ac0d7e7f19acc38e5944861b09243
mmamei <marco.mamei@unimore.it>
2026-03-12 12:37:18 +0100

    fix

--- example-service-account/main.py
+++ example-service-account/main.py
@@ -1,7 +1,13 @@
 from google.cloud import storage
-client = storage.Client.from_service_account_json('plcoud2024-afde155640c7.json')
+client = storage.Client.from_service_account_json('secret.json')
 #bucket = client.create_bucket('upload-mamei-1')
-bucket = client.bucket('pcloud2024-1')
+b  = 'pcloud2026-1'
+bucket = client.lookup_bucket(b)
+if bucket is None:
+    bucket = client.create_bucket(b)
+    print("Bucket {} creato.".format(b))
+else:
+    print("Bucket {} già esistente.".format(b))
 source_file_name = 'test.jpg'
 destination_blob_name = source_file_name
 blob = bucket.blob(destination_blob_name)
