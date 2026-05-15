78aed92f2148db64df8281b22a9e93a2dad9f357
mmamei <marco.mamei@unimore.it>
2026-05-15 15:31:14 +0200

    fix

---
+++ 15. CloudFunctions/query_data/main.py
@@ -0,0 +1,36 @@
+def save_data(request):
+    from google.cloud import firestore
+    import json
+    if request.method == 'OPTIONS':
+        print('------ options')
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600',
+            'Access-Control-Allow-Credentials': 'true'
+        }
+        return ('', 204, headers)
+
+    request_json = json.loads(request.values['data'])
+    N = int(request_json['N'])
+    
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+    db = firestore.Client(database='test1')
+
+    docs = db.collection('pm10').order_by('datetime', direction=firestore.Query.DESCENDING).limit(N).stream()
+
+    results = []
+    for doc in docs:
+        item = doc.to_dict()
+        item['id'] = doc.id
+        results.append(item['pm10'])
+
+    return (json.dumps(results), 200, headers)
+
+    
